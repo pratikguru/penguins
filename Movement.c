aa3ed8140cee3943b7ca2cf2e6d41f200659da53
Prateek gurudatt <pratik.gurudatt@gmail.com>
2016-11-22 12:56:46 +0100

    This has a couple of new functions for movements.c
    This code contains new functions declared for moving the penguins and checks before its a valid move or not with bool functions.

--- Movement.c
+++ Movement.c
@@ -1,20 +1,129 @@
 #include "Movement.h"
 
+/* These are the movement identifiers.
+Left [x-1][y]
+Right [x+1][y]
+Up-Left [x][y-1]
+Down-Left [x][y+1]
+Up-Right [x+1][y-1]
+Down-Right [x+1][y+1]
+*/
 Moves = 0;
 
-int getMoves(){
+
+bool CheckMoveValidity(){
+    // add code to check of move is valid or not.
+     return;
+}
+
+void MoveLeft(){                         /// this function controls the left movement of the penguin.
+ if (scanf == 'A'){                     /// This checks the corresponding move
+        if(CheckMoveValidity == true){  /// This checks the validity of the move.
+            return [x][y-1];            /// Returns the navigation commands to the function
+        }
+  else{
+    printf("Enter correct values");
+  }
+
+}
+ else {
+    printf("Invalid Moves");
+    }
+}
+
+void MoveRight(){                      /// this is similar to the above function.
+    if(scanf == 'D'){
+        if(CheckMoveValidity == true){
+        return [x+1][y];
+
+        }
+        else{
+        printf("Enter correct values");
+        }
+     }
+     else{
+        printf("Invalid Move");
+        }
+}
+
+
+void MoveUpLeft(){                                  /// this is similar to the above function
+    if(scanf == 'W + A'){
+        if(CheckMoveValidity == true){
+        return [x][y-1];
+        }
+        else {
+        printf("Enter correct Values!");
+        }
+    }
+    else {
+    Printf("Invalid Move");
+    }
+
+
+}
+
+void MoveUpRight(){
+ if (scanf == 'W + D'){
+    if (CheckMoveValidity == true){
+        return ([x+1][y-1]);
+    }
+    else{
+        printf("Enter correct Values");
+    }
+ else{
+ printf("Invalid Move");
+ }
+
+ }
+
+ void MoveDownRight(){
+if (scanf == 'S + D'){
+    if(CheckMoveValidity == true){
+    return [x+1][y+1];
+    }
+    else{
+    printf("Enter correct Values");
+    }
+}
+else{
+printf("Invalid move");
+}
+
+}
+
+
+void MoveDownLeft(){
+if(scanf == 'S + A'){
+    if(CheckMoveValidity ==true){
+        return [x][y+1];
+    }
+    else{
+    printf("Enter correct values");
+    }
+
+}
+else{
+printf("invalid Move");
+}
+}
+
+
+
+getMoves(){
     return Moves;
 };
 
-void defineMoves(){
+defineMoves(){
     scanf("%d", &Moves);
 }
 
-void setMoves(int number){
+setMoves(int number){
     Moves = number;
 };
 
-void Movement(){
+Movement(){
+    CollisionCheck();                       //for one or more penguins
     setMoves(getMoves() - 1);
     printf("Movement here\n");
 };
